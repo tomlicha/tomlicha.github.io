b53f972b5f8e21d698f95b27bb74f9592830c8d3
tomlicha <tomylicha@hotmail.fr>
2018-10-09 20:07:39 +0200

    test

---
+++ handler.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler();
+
+$validator = $pp->getValidator();
+$validator->fields(['Name','Email'])->areRequired()->maxLength(50);
+$validator->field('Email')->isEmail();
+$validator->field('Message')->maxLength(6000);
+
+
+
+
+$pp->sendEmailTo('t4licha@enib.fr'); // ← Your email here
+
+echo $pp->process($_POST);

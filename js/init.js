7f40a458cefb37a0d2a63f4b2af3164eb7bdc9d8
tomlicha <tomylicha@hotmail.fr>
2018-10-09 18:42:46 +0200

    test message js

--- js/init.js
+++ js/init.js
@@ -144,37 +144,14 @@
       var contactName = $('#contactForm #contactName').val();
       var contactEmail = $('#contactForm #contactEmail').val();
       var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
+      var contactMessage = $('#contactForm #contactMessage').val()+"\nemail\n"+contactEmail+"\n+name:\n"+contactName;
 
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+     
 
-      $.ajax({
+    $('#contactForm').attr('action',
+               'mailto:tomylicha@hotmail.fr?subject=' +
+               contactSubject + '&body=' +contactMessage);
 
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
-
-      });
-      return false;
-   });
 
 
 });
